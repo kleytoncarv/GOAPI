ecaed47576f7c2deb7b963b088cefcfa1d60204b
Kleyton Carv <kleyton_ky@hotmail.com>
2024-02-06 17:39:30 -0300

    Update main.go

--- cmd/catalog/main.go
+++ cmd/catalog/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/kleytoncarv/GOAPI/internal/database"
 	"github.com/kleytoncarv/GOAPI/internal/service"
@@ -26,7 +29,20 @@ func main() {
 	webProductHandler := webserver.NewWebProductHandler(productService)
 
 	c := chi.NewRouter()
+
+	c.Use(middleware.Logger)
+	c.Use(middleware.Recoverer)
+	
 	c.Get("/category/{id}", webCategoryHandler.GetCategory)
 	c.Get("/category", webCategoryHandler.GetCategories)
+	c.Post("/category", webCategoryHandler.CreateCategory)
+
+	c.Get("/product/{id}", webProductHandler.GetProduct)
+	c.Get("/product", webProductHandler.GetProducts)
+	c.Get("/product/category/{categoryID}", webProductHandler.GetProductByCategoryID)
+	c.Post("/product", webProductHandler.CreateProduct)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", c)
 
 }
